9a7be22e4fa9dd179b93b3f917ad4f4d314ce5e5
ngdkhoi <49549370+ngdkhoi@users.noreply.github.com>
2020-12-21 21:17:19 +0700

    format number

--- CakeShop/ViewModel/AddViewModel.cs
+++ CakeShop/ViewModel/AddViewModel.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace CakeShop.ViewModel
 {
     public class AddViewModel : BaseViewModel
     {
+        public int AddPrice { get => _AddPrice; set => _AddPrice = value; }
+        public string AddFormatedPrice { get => _AddFormatedPrice; set => _AddFormatedPrice = value; }
+
+        //public string AddFormatedPrice 
+        //{
+        //    get
+        //    {
+        //        return String.Format("Money {0, 0:C2}", 10000);
+        //    }
+        //    set 
+        //    { 
+        //        _AddFormatedPrice = value; 
+        //        OnPropertyChanged(); 
+        //    } 
+        //}
+
+        private int _AddPrice;
+        private string _AddFormatedPrice;
+        public AddViewModel()
+        {
+            CultureInfo cul = CultureInfo.GetCultureInfo("vi-VN");   // try with "en-US"
+            AddFormatedPrice = double.Parse("12345").ToString("#,###", cul.NumberFormat);
+        }
+
+        
     }
 }
