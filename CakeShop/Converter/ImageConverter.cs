12b48c9b99697743b59649110ee5bce3d2323cef
ngdkhoi <49549370+ngdkhoi@users.noreply.github.com>
2020-12-31 10:02:29 +0700

    converter image

---
+++ CakeShop/Converter/ImageConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CakeShop.ImageConverter
+{
+    public class AbsoluteConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string relative = (string)value;
+            string folder = Environment.CurrentDirectory.ToString();
+            string temp = System.IO.Directory.GetParent(folder).ToString();
+            temp = System.IO.Directory.GetParent(temp).ToString();
+            string absolutePath = $"{temp}{"\\Images/"}{relative}";
+
+            return absolutePath;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
