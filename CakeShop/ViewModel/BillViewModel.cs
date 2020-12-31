a1e4d1ef2372c3173e7a6cf15ed5f182cccfbb51
kevinpro2000 <kevinpro2000@gmail.com>
2020-12-31 20:33:20 +0700

    q

--- CakeShop/ViewModel/BillViewModel.cs
+++ CakeShop/ViewModel/BillViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CakeShop.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,11 @@ namespace CakeShop.ViewModel
 {
     public class BillViewModel :BaseViewModel
     {
+        public ObservableCollection<CAKE> List { get; set; } = new ObservableCollection<CAKE>(DataProvider.Ins.DB.CAKEs);
+
+        public BillViewModel()
+        {
+
+        }
     }
 }
